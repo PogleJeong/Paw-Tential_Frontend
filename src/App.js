12f4ed95a9fdf370371e66bd888db7c340d98269
jes <jes4958@yonsei.ac.kr>
2023-04-13 10:47:23 +0900

    write market-home-vlew(1)

--- src/App.js
+++ src/App.js
@@ -1,49 +1,44 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
 import './App.css';
 import Home from './component/Home.js';
 import Nav from './component/Nav.js';
+import MarketHome from './router/market/market-home';
 
 function App() {
   return (
     <div>
-    {/* 헤더 */}
-    <header>
-    <div>
-    <h1>PAW-TENTIAL</h1>
-    </div>
-    </header>
-
-
+      {/* 헤더 */}
+      <header>
+        <div>
+          <h1>PAW-TENTIAL</h1>
+        </div>
+      </header>
     
-    <BrowserRouter>
-    <div>
-    <nav>
-      {/* 사이드메뉴 */}
-      <div> 
-      <Nav />
-      </div>
-    </nav>
-    <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-
-      </Routes>
-
-      </main>
-      </div>
-    </BrowserRouter>
+      <BrowserRouter>
+      <div>
+        <nav>
+        {/* 사이드메뉴 */}
+          <div> 
+          <Nav />
+          </div>
+        </nav>
+        <main>
+          <Routes>
+            <Route path="/market-home" element={<MarketHome />} />
+          </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
     {/* footer */}
-    <footer>
+      <footer>
         <div>
           <p>
             <small>Copyright &copy;Paw-Tential</small>
           </p>
         </div>
       </footer>
-  </div>
+    </div>
   );
 
 }
