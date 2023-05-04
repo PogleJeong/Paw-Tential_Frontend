f2334e92ed7a82a9cd8e4a4c377a104b6edbd6b6
jes <jes4958@yonsei.ac.kr>
2023-05-04 14:00:38 +0900

    modify kakao GeoAPI

--- src/router/market/components/GeoAPI.js
+++ src/router/market/components/GeoAPI.js
@@ -1,47 +1,21 @@
 // 게시물 작성시 사용되는 Geo API
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
+
+import Geolocation from '@react-native-community/geolocation';
+
 const { kakao } = window;
-
-
-let initLat;
-let initLng;
-
-var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-};
-
-function success (pos) {
-    const crd = pos.coords;
-    initLat = crd.latitude;
-    initLng = crd.longitude;
-    
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-}
-
-function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-// React.memo 를 통해 rerendering 방지
 
 const KakaoMapWrite = ({ setGeoLat, setGeoLng }) => {
 
     const mapContentRef = useRef();
     const addressRef = useRef();
+    const infoRef = useRef();
 
     useEffect(()=>{
-        // 위치정보 먼저 불러온 후 실행
-        navigator.geolocation.getCurrentPosition(success, error);
-        // 추기 : 초기위치 정보 저장 : 지도정보를 사용하지 않은경우
-        console.log("위도: " + initLat + " | 경도 : " + initLng);
+        // 초기설정 - 일단 위도경도를 정해놓고 로드시 현재위치로 이동됨.
         const mapOption = {
-            center: new kakao.maps.LatLng(initLat, initLng), // 지도의 좌표
+            center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 좌표
             level: 3, // 지도의 확대레벨
         }
         const map = new kakao.maps.Map(mapContentRef.current, mapOption);
@@ -55,11 +29,29 @@ const KakaoMapWrite = ({ setGeoLat, setGeoLng }) => {
         const zoomControl = new kakao.maps.ZoomControl();
         map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
         */
-        // 기능추가 1. 주소-좌표 변환
-        var geocoder = new kakao.maps.services.Geocoder();
+        
+        let marker = new kakao.maps.Marker(), // 클릭한 위치를 표시할 마커입니다
+        infowindow = new kakao.maps.InfoWindow({zindex:1}); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
 
-        var marker = new kakao.maps.Marker(), // 클릭한 위치를 표시할 마커입니다
-            infowindow = new kakao.maps.InfoWindow({zindex:1}); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
+        // 현재 위치로 지도 이동하고 마커 표시;
+        if (navigator.geolocation) {
+            Geolocation.getCurrentPosition(
+                position => {
+                     // 현재 위치(위도, 경도) 가져오기
+                     const latitude = JSON.stringify(position.coords.latitude);
+                     const longitude = JSON.stringify(position.coords.longitude);
+ 
+                     // 지도 이동
+                     let currentPos = new window.kakao.maps.LatLng(latitude, longitude);
+                     map.panTo(currentPos);
+
+                     // 마커 표시
+                    marker.setPosition(latitude, longitude);
+                }
+            )
+        }  
+        // 기능추가 1. 주소-좌표 변환
+        let geocoder = new kakao.maps.services.Geocoder();
 
         // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
         searchAddrFromCoords(map.getCenter(), displayCenterInfo);
@@ -68,10 +60,10 @@ const KakaoMapWrite = ({ setGeoLat, setGeoLng }) => {
         kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
             searchDetailAddrFromCoords(mouseEvent.latLng, function(result, status) {
                 if (status === kakao.maps.services.Status.OK) {
-                    var detailAddr = !!result[0].road_address ? '<div>도로명주소 : ' + result[0].road_address.address_name + '</div>' : '';
+                    let detailAddr = !!result[0].road_address ? '<div>도로명주소 : ' + result[0].road_address.address_name + '</div>' : '';
                     detailAddr += '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
                     
-                    var content = '<div class="bAddr">' +
+                    let content = '<div class="bAddr">' +
                                     '<span class="title">법정동 주소정보</span>' + 
                                     detailAddr + 
                                 '</div>';
@@ -100,16 +92,15 @@ const KakaoMapWrite = ({ setGeoLat, setGeoLng }) => {
             // 좌표로 법정동 상세 주소 정보를 요청합니다
             geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
         }
-    
+        console.log("랜더링");
         // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
         function displayCenterInfo(result, status) {
             if (status === kakao.maps.services.Status.OK) {
-                var infoDiv = document.getElementById('centerAddr');
                 
-                for(var i = 0; i < result.length; i++) {
+                for(let i = 0; i < result.length; i++) {
                     // 행정동의 region_type 값은 'H' 이므로
                     if (result[i].region_type === 'H') {
-                        infoDiv.innerHTML = result[i].address_name;
+                        infoRef.current.innerText = result[i].address_name;
                         break;
                     }
                 }
@@ -129,7 +120,7 @@ const KakaoMapWrite = ({ setGeoLat, setGeoLng }) => {
             <span ref={addressRef}></span>
             <div ref={mapContentRef} style={{width: "500px", height: "500px"}}></div>
             <div>
-                <span id="centerAddr"></span>
+                <span ref={infoRef}></span>
             </div>
         </div>
     )
