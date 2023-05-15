bd455b0b4e5bbd312975435f2e2c0f6ddfe6719b
munkyungi <munkyungi@naver.com>
2023-05-15 14:54:32 +0900

    검색 수정 및 CSS

--- src/component/SearchUser.js
+++ src/component/SearchUser.js
@@ -21,16 +21,18 @@ const SearchUser = (prop) => {
     }
 
     // 검색어가 변경될 때마다 검색을 수행
-    const userId = prop.keyword.trim();
+    const userId = prop.keyword;
 
     fetchUsers(userId);
   }, [prop.keyword]);
 
   const fetchUsers = async (userId) => {
-    await axios.get('http://localhost:3000/userList', { params:{ "search": userId } })
-    .then(function(res){
-      console.log(res.data.list);
-      const filteredUsers = res.data.list.filter((user) =>
+    try {
+      const response = await axios.get('http://localhost:3000/userList', {
+        params: { search: userId }
+      });
+      console.log(response.data.list);
+      const filteredUsers = response.data.list.filter((user) =>
         user.id.includes(userId)
       );
 
@@ -73,17 +75,16 @@ const SearchUser = (prop) => {
         <div style={{ marginTop: '20px' }}>
           {users.map((user) => (
             <Card
-  key={user.id}
-  style={{ width: 200, display: 'inline-block', margin: '10px', cursor: 'pointer' }}
-  cover={<img alt="Profile" src={user.profile} />}
-  onClick={() => handleUserClick(user.id)}
->
-  <Meta
-    avatar={<Avatar src={user.profile} />}
-    title={user.id}
-    description={user.intro}
-  />
-</Card>
+              key={user.id}
+              style={{ width: 193, display: 'inline-block', marginTop: 10, cursor: 'pointer' }}
+              onClick={() => handleUserClick(user.id)}
+              >
+              <Meta
+                avatar={<Avatar src={user.profile} />}
+                title={user.id}
+                description={user.intro}
+              />
+            </Card>
 
           ))}
         </div>
